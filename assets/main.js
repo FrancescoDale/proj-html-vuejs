8c2d5fe32c42188d1e784ddd96c39bedfce4a16c
FrancescoDale <fd060862@gmail.com>
2020-11-30 17:57:15 +0100

    completata struttura navbar con un ciclo v-for che popola il menu

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,14 @@
 var app = new Vue ({
     el : '#root',
+    data : {
+        navbarMenu : [
+            'home',
+            'courses',
+            'about us',
+            'news',
+            'pages',
+            'contact',
+            'purchase',
+        ],
+    },
 });
